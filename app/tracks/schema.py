c702deb5951e2d8c2a0e78804330e4423aef849b
cache-monet <remiaston@gmail.com>
2019-05-17 20:16:59 -0400

    added feature updateTracks

--- app/tracks/schema.py
+++ app/tracks/schema.py
@@ -20,7 +20,6 @@ class CreateTrack(graphene.Mutation):
         artist = graphene.String()
         album = graphene.String()
         url = graphene.String()
-    # def mutate(self, info, title, album, artist, description, url):
     def mutate(self, info, **kwargs):
         user = info.context.user
         if user.is_anonymous:
@@ -36,5 +35,33 @@ class CreateTrack(graphene.Mutation):
         track.save()
         return CreateTrack(track=track)
 
+class UpdateTrack(graphene.Mutation):
+    track = graphene.Field(TrackType)
+
+    class Arguments:
+        track_id = graphene.Int(required=True)
+        title = graphene.String()
+        description = graphene.String()
+        artist = graphene.String()
+        album = graphene.String()
+        url = graphene.String()
+
+    def mutate(self, info, **kwargs):
+        user = info.context.user
+        track = Track.objects.get(id=kwargs.get('track_id'))
+
+        if user.is_anonymous:
+            raise Exception("You must be logged in to update tracks")
+
+        if kwargs.get('title'): track.title = kwargs.get('title')
+        if kwargs.get('description'): track.description = kwargs.get('description')
+        if kwargs.get('artist'): track.artist = kwargs.get('artist')
+        if kwargs.get('album'): track.album = kwargs.get('album')
+        if kwargs.get('url'): track.url = kwargs.get('url')
+
+        track.save()
+        return UpdateTrack(track=track)
+
 class Mutation(graphene.ObjectType):
     create_track = CreateTrack.Field()
+    update_track = UpdateTrack.Field()
